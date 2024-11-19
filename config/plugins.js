b7e08dfa66e59cae45e3f7fefa836e1030215587
Rahul <rahulyadav1209ts@gmail.com>
2024-11-19 14:45:59 +0530

    file upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION,
+        params: {
+          Bucket: process.env.AWS_BUCKET_NAME,
+        },
+      },
+    },
+  },
+};
